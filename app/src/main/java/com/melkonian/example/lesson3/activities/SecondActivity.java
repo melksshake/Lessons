514f826971fbd222480598238295b7bb7383d303
Nataliia <nat-melk@yandex.ru>
2019-07-03 01:21:39 +0300

    Migrated to androidx.

--- app/src/main/java/com/melkonian/example/lesson3/activities/SecondActivity.java
+++ app/src/main/java/com/melkonian/example/lesson3/activities/SecondActivity.java
@@ -3,12 +3,11 @@ package com.melkonian.example.lesson3.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatButton;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import com.melkonian.example.lesson3.R;
 import com.melkonian.example.lesson3.model.LocalParcel;
 
